317cb1db520c8041eadbd3b0f5f1038eeba9d44e
Rui Barbosa Martins <ruibm@fb.com>
2016-09-29 15:22:06 +0100

    Initial revision.

---
+++ sync_dir_remotely.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+#
+# This program continuously one-way synchronises one local directory
+# into a remote machine.
+#
+# LocalClient ==> RemoteServer
+#
+# Usage:
+#
+
+
+
+#########################################################
+# Constants
+#########################################################
+
+
+
+#########################################################
+# Classes
+#########################################################
+class RemoteServer(object):
+  pass
+
+
+class LocalClient(object):
+  pass
+
+
+
+#########################################################
+# Functions
+#########################################################
+def main():
+  print 'Working!!!'
+
+
+
+if __name__ == '__main__':
+  main()
+
